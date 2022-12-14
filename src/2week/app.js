45a17118e80d31f39748bb458abf3b4ccb58ad0c
dahye1013 <alrean@naver.com>
2022-12-14 23:52:40 +0900

    refactor: 3주차 함수형 코딩 라이브쉐어 페어프로그래밍 진행

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,6 +1,7 @@
-var shopping_cart = [];
-var shopping_cart_total = 0;
+// A
+const shopping_cart = [];
 
+// A
 document.querySelectorAll('button').forEach(button =>
   button.addEventListener('click', ({ target }) => {
     const name = target.parentNode.querySelector('.menu-name').textContent;
@@ -11,42 +12,49 @@ document.querySelectorAll('button').forEach(button =>
   }),
 );
 
-function add_item_to_cart(item) {
-  shopping_cart.push(item);
-  console.log(shopping_cart);
-  calc_cart_total();
+
+// A
+const add_item_to_cart = (item) => {
+  const next_cart = add_item(shopping_cart, item);
+  calc_cart_total(next_cart);
 }
 
-function calc_cart_total() {
-  shopping_cart_total = 0;
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
+// A
+const calc_cart_total = (cart) => {
+  const shopping_cart_total = calc_cart_total_price(cart);
+
+  update_shipping_icons(shopping_cart_total);
+  set_cart_total_dom(shopping_cart_total);
+  update_tax_dom(shopping_cart_total);
+}
+
+// A
+const set_cart_total_dom = (cart_total) => {
+  document.querySelector('.total-price').textContent = `${cart_total}원`;
+}
+
+// A
+const update_shipping_icons = (cart_total) => {
+  const buy_buttons = get_buy_buttons_dom();
+
+  for (let i = 0; i < buy_buttons.length; i++) {
+    const item = buy_buttons[i];
+    const price = get_cart_price(item);
+    const next_total = add(cart_total, price);
+
+
+    gets_free_shipping(next_total, FREE_SHIPPING_PRICE)
+      ? item.show_free_shopping_icon()
+      : item.hide_free_shopping_icon();
   }
-  set_cart_total_dom();
-  update_shipping_icons();
-  update_tax_dom();
 }
 
-function set_cart_total_dom() {
-  document.querySelector('.total-price').textContent = shopping_cart_total;
-}
+// A
+const get_buy_buttons_dom = () => {
+  const buttons = [];
 
-function update_shipping_icons() {
-  var buy_buttons = get_buy_buttons_dom();
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var item = buy_buttons[i];
-    console.log(item);
-    if (item.price + shopping_cart_total >= 20) item.show_free_shopping_icon();
-    else item.hide_free_shopping_icon();
-  }
-}
-
-function get_buy_buttons_dom() {
-  var buttons = [];
-
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
+  for (let i = 0; i < shopping_cart.length; i++) {
+    const item = shopping_cart[i];
     item.show_free_shopping_icon = function () {
       console.log('DOM 의 아이콘을 보여줍니다');
     };
@@ -59,10 +67,42 @@ function get_buy_buttons_dom() {
   return buttons;
 }
 
-function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.1);
+// A
+const update_tax_dom = (calc_total) => {
+  set_tax_dom(calc_total * TAX_SCALE);
 }
 
-function set_tax_dom(value) {
+// A
+const set_tax_dom = (value) => {
   document.querySelector('.total-price').textContent = value;
 }
+
+
+// C - shipping
+const gets_free_shipping = (addedPrice, freeShippingPrice) => addedPrice >= freeShippingPrice;
+
+// C - cart
+const get_cart_price_list = (cart) => cart.map(item => get_cart_price(item));
+const get_cart_price = ({ price }) => price;
+
+// C - cart
+const calc_cart_total_price = cart => sum_array(get_cart_price_list(cart));
+
+// C - cart
+const add_item = (cart, item) => add_element_to_array(cart, item);
+
+// C - item
+const calc_added_item = (total, item) => add(total, item.price);
+
+// C - util
+const sum_array = (numArray) => numArray.reduce(add, 0);
+
+// C - util
+const add_element_to_array = (array, element) => [...array, element];
+
+// C - util
+const add = (num1, num2) => num1 + num2;
+
+// D
+const FREE_SHIPPING_PRICE = 20000;
+const TAX_SCALE = 0.1;
