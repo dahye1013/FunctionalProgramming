1aff00d8bff80d1345d6e8a4d36e3235888f10e1
dahye1013 <alrean@naver.com>
2022-12-15 19:47:54 +0900

    style: eslint 적용

--- src/2week/app.js
+++ src/2week/app.js
@@ -12,42 +12,39 @@ document.querySelectorAll('button').forEach(button =>
   }),
 );
 
-
 // A
-const add_item_to_cart = (item) => {
+const add_item_to_cart = item => {
   const next_cart = add_item(shopping_cart, item);
   calc_cart_total(next_cart);
-}
+};
 
 // A
-const calc_cart_total = (cart) => {
+const calc_cart_total = cart => {
   const shopping_cart_total = calc_cart_total_price(cart);
 
   update_shipping_icons(shopping_cart_total);
   set_cart_total_dom(shopping_cart_total);
   update_tax_dom(shopping_cart_total);
-}
+};
 
 // A
-const set_cart_total_dom = (cart_total) => {
+const set_cart_total_dom = cart_total => {
   document.querySelector('.total-price').textContent = `${cart_total}원`;
-}
+};
 
 // A
-const update_shipping_icons = (cart_total) => {
+const update_shipping_icons = cart_total => {
   const buy_buttons = get_buy_buttons_dom();
 
   for (let i = 0; i < buy_buttons.length; i++) {
     const item = buy_buttons[i];
     const price = get_cart_price(item);
     const next_total = add(cart_total, price);
-
-
     gets_free_shipping(next_total, FREE_SHIPPING_PRICE)
       ? item.show_free_shopping_icon()
       : item.hide_free_shopping_icon();
   }
-}
+};
 
 // A
 const get_buy_buttons_dom = () => {
@@ -65,24 +62,23 @@ const get_buy_buttons_dom = () => {
   }
 
   return buttons;
-}
+};
 
 // A
-const update_tax_dom = (calc_total) => {
+const update_tax_dom = calc_total => {
   set_tax_dom(calc_total * TAX_SCALE);
-}
+};
 
 // A
-const set_tax_dom = (value) => {
+const set_tax_dom = value => {
   document.querySelector('.total-price').textContent = value;
-}
-
+};
 
 // C - shipping
 const gets_free_shipping = (addedPrice, freeShippingPrice) => addedPrice >= freeShippingPrice;
 
 // C - cart
-const get_cart_price_list = (cart) => cart.map(item => get_cart_price(item));
+const get_cart_price_list = cart => cart.map(item => get_cart_price(item));
 const get_cart_price = ({ price }) => price;
 
 // C - cart
@@ -95,7 +91,7 @@ const add_item = (cart, item) => add_element_to_array(cart, item);
 const calc_added_item = (total, item) => add(total, item.price);
 
 // C - util
-const sum_array = (numArray) => numArray.reduce(add, 0);
+const sum_array = numArray => numArray.reduce(add, 0);
 
 // C - util
 const add_element_to_array = (array, element) => [...array, element];
