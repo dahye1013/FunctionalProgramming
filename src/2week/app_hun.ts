e26d17449577a2c7f01527ae9238194e464102a6
Hun <hun@nextunicorn.kr>
2022-12-18 20:36:39 +0900

    Chore: 주석

--- src/2week/app_hun.ts
+++ src/2week/app_hun.ts
@@ -23,12 +23,13 @@ let shopping_cart: CartItem[] = [];
 document.querySelectorAll("button").forEach((button) =>
   button.addEventListener("click", ({ target: aTarget }) => {
     const target = aTarget as HTMLButtonElement;
-    const item = get_item_from_element(target);
+    const item = get_item_from_add_button_element(target);
     add_item_to_cart(item);
   })
 );
 
 // C - item
+// 타입 체킹이 포함된 make_item을 만드는 함수
 const make_item = ({
   name: aName,
   category: aCategory,
@@ -52,7 +53,8 @@ const make_item = ({
 };
 
 // A
-const get_item_from_element = (element: HTMLElement) => {
+// add button element dom에서 item을 만들기 위한 값을 빼내어 item object 제작
+const get_item_from_add_button_element = (element: HTMLElement) => {
   const name = element.parentNode.querySelector(".menu-name").textContent;
   const category = element.parentNode.querySelector(".category").textContent;
   const price = element.parentNode.querySelector(".price").textContent;
@@ -109,16 +111,19 @@ const update_shipping_icons = (cart: CartItem[], cart_total: number) => {
 };
 
 // A
+// 무료배송 버튼이 나타날때 상품 name과 함께 콘솔 출력
 const show_free_shopping_icon = (name: string) => {
   console.log(`'${name}' 주문시 무료배송!`);
 };
 
 // A
+// 무료배송 버튼이 숨겨질때 상품 name과 함께 콘솔 출력
 const hide_free_shopping_icon = (name: string) => {
   console.log(`'${name}' 주문시 무료배송 취소ㅠ`);
 };
 
 // A
+// Button 타입은 item 타입에 버튼을 노출/비노출 하는 두 개의 함수가 추가된 것과 같음
 const get_buy_buttons_dom = (cart: CartItem[]) => {
   const buttons: Button[] = [];
 
@@ -165,10 +170,12 @@ const add_item = (cart: CartItem[], item: CartItem) =>
   add_element_to_array(cart, item);
 
 // C - util
+// number 1000000 -> string 1,000,000
 const convert_number_to_price_format = (price: number) =>
   `${price.toLocaleString()}원`;
 
 // C - util
+// '1000000원' -> '1000000'
 const convert_string_to_number_only = (str: string) =>
   str.replace(/[^0-9]/g, "");
 
