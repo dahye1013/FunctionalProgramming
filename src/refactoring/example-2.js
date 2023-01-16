33ac5f15095c75df0ea46f82c874c7ce21e39ac1
dahye1013 <alrean@naver.com>
2023-01-17 00:17:09 +0900

    feat: forEach 구현

--- src/refactoring/example-2.js
+++ src/refactoring/example-2.js
@@ -1,12 +1,27 @@
-for (var i = 0; i < foods.length; i++) {
-  var food = foods[i];
+const cook = (food) => {}
+const eat = (food) => {}
+const wash = (dish) => {}
+const dry = (dish) => {}
+const putAway = (dish) => {}
+
+const foods = ['치킨', '피자']
+forEach(foods, (food) => {
   cook(food);
   eat(food);
-}
+})
 
-for (var i = 0; i < dishes.length; i++) {
-  var dish = dishes[i];
+const dishes = [1,2,3,4,5]
+forEach(dishes, () => {
   wash(dish);
   dry(dish);
   putAway(dish);
+
+})
+
+function forEach(array, callback) {
+  if(!Array.isArray(array)) throw new Error('배열 인스턴스를 첫 번째 인자로 넣어주세요.')
+  for (let index = 0; index < array.length; index++) {
+      const el = array[index];
+      callback(el, index, array)
+  }
 }
